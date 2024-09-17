8bacc2eb19ac2cdcc3c1989ccd2a54d43a6bb76d
coseemo <cosimo.borghini1@edu.unifi.it>
2024-09-18 01:08:52 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/cosim/CLionProjects/astarsearch/tests/lib
-# Build directory: C:/Users/cosim/CLionProjects/astarsearch/cmake-build-debug/tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
