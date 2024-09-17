7e8a4a5202a9b3ca4a711e14fd3e886cad9e0f01
coseemo <cosimo.borghini1@edu.unifi.it>
2024-09-18 01:03:36 +0200

    corretto incapsulamento e costanza dei metodi, aggiunto unit testing

---
+++ cmake-build-debug/tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/cosim/CLionProjects/astarsearch/tests/lib/googletest
+# Build directory: C:/Users/cosim/CLionProjects/astarsearch/cmake-build-debug/tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
